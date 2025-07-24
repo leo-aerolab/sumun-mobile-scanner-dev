0e2f21a21c887bc1d6ea74d99c5532201df41d98
Leo <leonardo.garciacuenca@aerolab.co>
2025-07-24 16:10:35 -0300

    fix

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'standalone',
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      path: false,
+      os: false,
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
