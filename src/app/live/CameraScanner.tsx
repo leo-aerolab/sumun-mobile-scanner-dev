dc05ed5361e1671f2baad7561dcdc08e5400df3f
Leo <leonardo.garciacuenca@aerolab.co>
2025-08-14 19:07:23 -0300

    test3

--- src/app/live/CameraScanner.tsx
+++ src/app/live/CameraScanner.tsx
@@ -667,7 +667,7 @@ export const CameraScanner: React.FC = () => {
           />
           {/* Field blocks image on top */}
           <div className="absolute inset-0 px-6 bg-black/40 flex items-start flex-col">
-            <div className="flex flex-1/2 justify-center items-end w-full h-7/12">
+            <div className="flex flex-1/2 justify-center items-end w-full h-[55%]">
               {/* eslint-disable-next-line @next/next/no-img-element */}
               <img
                 src={capturedImage.fieldBlocksImage}
@@ -675,9 +675,7 @@ export const CameraScanner: React.FC = () => {
                 className="max-w-md max-h-4/5 mx-auto h-auto object-cover rounded-2xl"
               />
             </div>
-            <div className="flex flex-1/2 h-5/12">
-              {/* spacing */}
-            </div>
+            <div className="flex flex-1/2 h-[45%]"></div>
           </div>
         </>
       )}
